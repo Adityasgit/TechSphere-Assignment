50a096c578450c9100a858cfb3e5d43e0b42e23a
Aditya <adityaaroraaditya12@gmail.com>
2024-05-18 18:58:53 +0530

    added mute and download

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function convertSecondsToMinutes(seconds) {
 function App() {
   const tracksLoadable = useRecoilValueLoadable(fetchSongs);
   const [currId, setCurrId] = useState(null);
+  const [currTab, setCurrTab] = useState(tabsData[0]);
   const [currentSong, setCurrentSong] = useRecoilState(currentSongState);
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
@@ -95,19 +96,19 @@ function App() {
         <div className='hidden text-center h-full w-full md:flex flex-col justify-between'>
           <div><img className='p-3 invert-[100%]' src={logo} alt="Spotify LOGO" /></div>
           <div className='h-full text-left p-6 flex gap-2 flex-col text-xl font-semibold text-gray-400'>
-            {tabsData.map((tab, i) => (<div key={i} style={{ cursor: "pointer" }} className={i === 0 ? "text-white" : ""}>{tab}</div>))}
+            {tabsData.map((tab, i) => (<div onClick={() => setCurrTab(tab)} key={i} style={{ cursor: "pointer" }} className={currTab === tab ? "text-white" : ""}>{tab}</div>))}
           </div>
           <div className='text-left p-3'><img className='w-[4vmax] h-[4vmax] object-cover rounded-[50%] mx-4 mb-2' src={avatar} alt="avatar" /></div>
         </div>
       }
       content2={
         <div className='hidden h-full w-full md:flex flex-col justify-between'>
-          <div className='p-4 mt-3 font-bold text-3xl'>For You</div>
+          <div className='p-4 mt-3 font-bold text-3xl'>{currTab}</div>
           <div className='relative'>
             <input type='text' className='p-3 rounded-lg bg-[#383737] w-[85%] pr-12 text-sm m-auto' placeholder='Search Song, Artist' />
             <div className="absolute top-[7px] right-[70px]"><SearchIcon className="text-xl" fontSize='medium' /></div>
           </div>
-          <div className='mt-4 w-full flex overflow-scroll flex-col justify-center'>
+          {currTab === "For You" && <div className='mt-4 w-full flex overflow-scroll flex-col justify-center'>
             {tracksLoadable.state === 'loading' ? <div className='w-[100%] h-full text-center p-6'>Loading...</div> : tracksLoadable.state === 'hasValue'
               ? tracksLoadable.contents.data.map(
                 (track, i) => (
@@ -122,7 +123,7 @@ function App() {
                     <div className='w-[10%] text-gray-300'>{convertSecondsToMinutes(track.duration)}</div>
                   </div>))
               : <div>Error</div>}
-          </div>
+          </div>}
         </div>
       }
       content3={
